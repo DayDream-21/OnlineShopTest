2c43b93336c5f08708a78f4ab89f750b915ba43c
DayDream-21 <vova13158@gmail.com>
2023-03-17 21:10:48 +0300

    Initial commit

---
+++ src/main/java/com/slavamashkov/onlineshoptest/OnlineShopTestApplication.java
@@ -0,0 +1,11 @@
+package com.slavamashkov.onlineshoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/slavamashkov/onlineshoptest/OnlineShopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.onlineshoptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
